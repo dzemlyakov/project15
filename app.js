d9d412103b748a9de061dfca1988b5d9f9f1b749
Дмитрий <zewsekus@gmail.com>
2020-02-05 19:50:56 +0300

    Connect to Mongo

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const path = require('path');
+const mongoose = require('mongoose');
 const cardsRouter = require('./routes/cards');
 const usersRouter = require('./routes/users');
 
@@ -7,6 +8,12 @@ const app = express();
 
 
 const { PORT = 3000 } = process.env;
+mongoose.connect('mongodb://localhost:27017/mestodb', {
+  useNewUrlParser: true,
+  useCreateIndex: true,
+  useFindAndModify: false,
+});
+
 
 app.use(express.static(path.join(__dirname, 'public')));
 app.use('/cards', cardsRouter);
